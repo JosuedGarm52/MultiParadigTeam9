edcb44cc0645a5cdc4d6fe9df41295a9326c4e5f
= <=>
2023-09-14 12:06:43 -0500

    U1 - Equipo9 - Práctica 5

---
+++ Practica1/p5/U1-Equipo9-Ejercicio5.py
@@ -0,0 +1,24 @@
+# #5 – Manejo de información
+# INSTRUCCIONES: Escribir una función que reciba n parámetros de llave valor e imprima la información en formato
+# “{valor}”: “{llave}”.
+
+# Alejandro Díaz Medrano                19100168
+# Josué Daniel García Maldonado         19100182
+# Rogelio Zamarripa Treviño             18100248
+
+def funcionImprimirValoresLlaves(n):
+    llaves = {}
+    i = 1
+
+    while i <= n:
+        llave = input(f"Ingrese la llave {i}: ")
+        valor = int(input(f"Ingrese el valor de llave {i}: "))
+
+        llaves[llave] = valor
+        i+=1
+    
+    for llave, valorLlave in llaves.items():
+        print(f"{valorLlave} : {llave}")
+
+n = int(input("Ingrese la cantidad de datos que desea registar: "))
+funcionImprimirValoresLlaves(n)
