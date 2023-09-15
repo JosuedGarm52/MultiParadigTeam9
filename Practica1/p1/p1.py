f678365ab9939df70d8dba93723d84c68179f117
Josue <jaccobjosue@gmail.com>
2023-09-14 20:20:09 -0500

    Alejandro parte

---
+++ Practica1/p1/p1.py
@@ -0,0 +1,23 @@
+# Definir la función que calcula el producto y la suma total
+def calcular_producto_y_suma(*args):
+    producto_total = 1  # Inicializamos el producto en 1
+    suma_total = 0      # Inicializamos la suma en 0
+    
+    for numero in args:
+        producto_total *= numero
+        suma_total += numero
+    
+    return producto_total, suma_total
+
+# Solicitar al usuario la entrada de números separados por comas
+entrada = input("Ingresa números separados por comas: ")
+
+# Convertir la entrada en una lista de números
+numeros = [float(numero) for numero in entrada.split(',')]
+
+# Llamar a la función con los números proporcionados
+producto, suma = calcular_producto_y_suma(*numeros)
+
+# Mostrar los resultados
+print(f"El producto total es: {producto}")
+print(f"La suma total es: {suma}")
---
+++ Practica1/p8/p8.py
@@ -0,0 +1,107 @@
+#libreria random
+import random
+
+#clase para crear usuarios
+
+class Usuario:
+    def __init__(self, usuario, contrasena, rol, nombre, curp, ciudad) -> None:
+        self._usuario = usuario
+        self._contrasena = contrasena
+        self._rol = rol
+        self._nombre = nombre
+        self._curp = curp
+        self._ciudad = ciudad
+#propiedades necesarias para la comprobacion de informacion, solo mostrar
+    @property
+    def usuario(self):
+        return self._usuario
+    @property
+    def contrasena(self):
+        return self._contrasena
+    @property
+    def rol(self):
+        return self._rol
+    def __str__(self) -> str:
+        return f"{self._usuario}, {self._contrasena}, {self._rol}, {self._nombre}, {self._curp}, {self._ciudad}"
+
+#self, usuario, contrasena, rol, nombre, curp, ciudad
+#usuarios base para iniciar el admin y un cliente
+miUsuario = Usuario("alex", "nan", "admin", "alejandro", "DIM", "NLD")
+dicUsuarios = {0:miUsuario,}
+miUsuario = Usuario("ella", "nan", "cliente", "alejandro", "DIMas", "NLD")
+dicUsuarios[5]= miUsuario
+
+def registrar():
+    while True:
+        usuario = input('Ingresa tu nuevo usuario: ')
+        contrasena = input('Ingresa tu nueva contrasena: ')
+        nombre = input('ingresa el nombre del usuario: ')
+        while True:
+            curp = input('ingresa la CURP del usuario: ')
+            if curp in dicUsuarios:
+                print('El usuario ya existe')
+                continue
+            else:
+                break
+        ciudad = input('ingresa la ciudad del usuario: ')
+        print(usuario, contrasena, nombre, curp, ciudad)
+        if usuario != None and contrasena != None and nombre != None and curp != None and ciudad != None:
+            miUsuario = Usuario(usuario, contrasena, "cliente", nombre, curp, ciudad)
+            key = 0
+			#asignacion de llave para el diccionario random y evita repetidos 
+			#para no modificar el valor
+            while True:
+                numRamdom = random.randint(0,100)
+                if numRamdom in dicUsuarios:
+                    continue
+                else:
+                    key = numRamdom
+                    break
+            dicUsuarios[key] = miUsuario
+            break
+        else:
+            print('datos incorrectos')  
+            res = int(input('deseas volver a registrarte?\n1.-Si\n2.-No'))
+            if res == 1:
+                continue
+            else:
+                break
+
+def Iniciar():
+    for k,v in dicUsuarios.items():
+        print(k,v,"\n")
+    bandera = True
+    while bandera == True:
+        usu = input('ingresa usuario: ')
+        cont = input('ingresa contrasena: ')
+        for k,v in dicUsuarios.items(): #primer ciclado para recorrer las cuentas
+            if usu == v.usuario and cont == v.contrasena:
+                if v.rol == 'admin':
+                    for k2,v2 in dicUsuarios.items():#segundo diccionario para imprimir
+						#en caso de ingresar con la cuenta admin
+                        print(v2,"\n")
+                    bandera = False
+                    break
+                else:
+					#mensaje de ingreso
+                    print(f'el usuario  "{v.usuario}"  ha logrado ingresar')
+                    bandera = False
+                    break
+        if bandera == True:
+            res = int(input('quieres volver a intentar\n1.-Si\n2.-No\nSeleccion: '))
+            if res == 1:
+                bandera = True
+                continue
+            else:
+                bandera = False
+                break
+    return
+
+while True:
+    res = int(input('Menu\n1.-Registro\n2.-Inicio de sesion\n3.-Salida\nseleccion: '))
+    if res == 1:
+        registrar()
+    elif res == 2:
+        Iniciar()
+    else:
+        quit()
