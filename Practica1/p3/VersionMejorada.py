b382bb606a58d71fccbf47354b6e075fbcea125a
Josue <jaccobjosue@gmail.com>
2023-09-16 20:36:55 -0500

    correccion

--- Practica1/p3/VersionMejorada.py
+++ Practica1/p3/VersionMejorada.py
@@ -3,9 +3,12 @@ nombre_completo = input("Por favor, introduce tu nombre completo: ")
 nombre_formateado = ""
 
 for i, letra in enumerate(nombre_completo):
-    if i % 2 == 0:
-        nombre_formateado += letra.upper()
+    if letra.isalpha():
+        if i % 2 == 0:
+            nombre_formateado += letra.upper()
+        else:
+            nombre_formateado += letra.lower()
     else:
-        nombre_formateado += letra.lower()
+        nombre_formateado += letra
 
 print("Tu nombre con letras intercaladas es:", nombre_formateado)
--- Practica1/p3/p3.py
+++ Practica1/p3/p3.py
@@ -5,11 +5,14 @@ nombre_inverso = ""
 mayuscula = True
 
 for letra in nombre_completo:
-    if mayuscula:
-        nombre_inverso += letra.upper()
+    if letra.isalpha():
+        if mayuscula:
+            nombre_inverso += letra.upper()
+        else:
+            nombre_inverso += letra.lower()
+        mayuscula = not mayuscula
     else:
-        nombre_inverso += letra.lower()
-    mayuscula = not mayuscula
+        nombre_inverso += letra
 
-print("Su nombre de manera inversa letra por letra intercalando"+
-     + "una letra minuscula a una mayuscula es: ", nombre_inverso)
+print("Su nombre de manera inversa letra por letra intercalando "
+     + f"una letra minuscula a una mayuscula es: {nombre_completo} : {nombre_inverso}")
