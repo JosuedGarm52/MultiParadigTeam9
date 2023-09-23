ba7553febcd1fe197bc0b07307e4f4b0f2217c55
Josue <jaccobjosue@gmail.com>
2023-09-23 12:53:17 -0500

    Cambio p3

--- Practica1/p3/VersionMejorada.py
+++ Practica1/p3/VersionMejorada.py
@@ -1,14 +1,10 @@
-nombre_completo = input("Por favor, introduce tu nombre completo: ")
+nombre_completo = input("Introduce tu nombre: ")
 
-nombre_formateado = ""
+nombre_inverso = ''.join(
+    (letra.lower() if i % 2 == 0 else letra.upper()) + ' '
+    if letra.isalpha()
+    else letra
+    for i, letra in enumerate(reversed(nombre_completo))
+)
 
-for i, letra in enumerate(nombre_completo):
-    if letra.isalpha():
-        if i % 2 == 0:
-            nombre_formateado += letra.upper()+' '
-        else:
-            nombre_formateado += letra.lower()+' '
-    else:
-        nombre_formateado += letra
-
-print("Tu nombre con letras intercaladas es:", nombre_formateado.strip())
+print(f"Su nombre de manera inversa letra por letra intercalando una letra minuscula a una mayuscula es: {nombre_completo} : {nombre_inverso[::-1].strip()}")
--- Practica1/p3/p3.py
+++ Practica1/p3/p3.py
@@ -4,15 +4,15 @@ nombre_completo = input("Introduce tu nombre: ")
 nombre_inverso = ""
 mayuscula = True
 
-for letra in nombre_completo.lower():
+for letra in nombre_completo[::-1].lower():
     if letra.isalpha():
         if mayuscula:
-            nombre_inverso += (letra.upper()+' ')
+            nombre_inverso += (letra.lower()+' ')
         else:
-            nombre_inverso += (letra.lower()+ ' ')
+            nombre_inverso += (letra.upper()+ ' ')
         mayuscula = not mayuscula
     else:
         nombre_inverso += letra
 
 print("Su nombre de manera inversa letra por letra intercalando "
-     + f"una letra minuscula a una mayuscula es: {nombre_completo} : {nombre_inverso.strip()}")
+     + f"una letra minuscula a una mayuscula es: {nombre_completo} : {nombre_inverso[::-1].strip()}")
