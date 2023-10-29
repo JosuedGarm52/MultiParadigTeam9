4b4dada9e3c2174037c4216a0d6f3b1c969af783
Josue <jaccobjosue@gmail.com>
2023-10-29 15:17:50 -0500

    parte 3 correcion

--- PracticasQQ/Practica2/Pract2/myapp/myapp/settings.py
+++ PracticasQQ/Practica2/Pract2/myapp/myapp/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pract2',
+        'USER': 'postgres',
+        'PASSWORD': 'contra',
+        'HOST': 'localhost', 
+        'PORT': '5433',           
     }
 }
 
