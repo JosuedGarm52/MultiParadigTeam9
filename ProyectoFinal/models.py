6692789e2a8e7dd85cfba269f406ac678f99a037
Josue <jaccobjosue@gmail.com>
2023-11-21 15:38:36 -0600

    models pt2

--- ProyectoFinal/models.py
+++ ProyectoFinal/models.py
@@ -58,6 +58,7 @@ class Perfil(db.Models):
         self.cuenta_id = cuenta_id
     
 class Foto(db.Models):
+    __tablename__="foto"
     id_foto=db.Column(db.Integer,primary_key = True, autoincrement = True)
     link = db.Column(db.String(255), nullable=False)#req
 
@@ -65,3 +66,86 @@ class Foto(db.Models):
         self.id_foto = id_foto
         self.link = link
 
+class Perfil_Foto(db.Models):
+    __tablename__= "perfil_foto"
+    usuario_name = db.Column(db.Integer, db.ForeignKey('perfil.usuario'), primary_key=True)
+    foto_id = db.Column(db.Integer, db.ForeignKey('foto.id_foto'), primary_key=True)
+    isperfil = db.Column(db.Boolean, nullable=False, default=True)#req
+    
+    usuario = db.relationship('Perfil', backref=db.backref('perfil_fotos', lazy=True))
+    
+    foto = db.relationship('Foto', backref=db.backref('perfil_fotos', lazy=True))
+
+    def __init__(self, usuario_name, foto_id, isperfil=True) -> None:
+        self.usuario_name = usuario_name
+        self.foto_id = foto_id
+        self.isperfil = isperfil
+
+class Admin(db.Models):
+    __tablename__= "admin"
+    id_admin = db.Column(db.Integer,primary_key = True, autoincrement = True)
+    cuenta_id = db.Column(db.Integer, db.ForeignKey('cuenta.id_cuenta'), nullable=False)
+    
+    def __init__(self, id_admin,cuenta_id):
+        self.id_admin = id_admin
+        self.cuenta_id = cuenta_id
+    
+class Mod(db.Models):
+    __tablename__= "mod"
+
+    id_mod = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    cuenta_id = db.Column(db.Integer, db.ForeignKey('cuenta.id_cuenta'), nullable=False)
+    cuenta = db.relationship('cuenta', backref=db.backref('mod', lazy=True))
+
+    def __init__(self, id_cuenta,cuenta) -> None:
+        self.cuenta_id = id_cuenta #checar esta parte
+        self.cuenta = cuenta
+
+class Mensaje(db.Model):
+    __tablename__ = "mensaje"
+    id_mensaje = db.Column(db.Integer, primary_key=True)
+    usuario_rem = db.Column(db.String(255), db.ForeignKey('perfil.usuario'), nullable=False)
+    usuario_dest = db.Column(db.String(255), db.ForeignKey('perfil.usuario'), nullable=False)
+    fecha = db.Column(db.Date, nullable=False)
+    contenido = db.Column(db.String, nullable=False)
+    isvisible = db.Column(db.Boolean, nullable=False, default=True)
+    isread = db.Column(db.Boolean, nullable=False, default=False)
+    
+    remitente = db.relationship('perfil', foreign_keys=[usuario_rem])
+    destinatario = db.relationship('perfil', foreign_keys=[usuario_dest])
+
+    def __init__(self, id_mensaje, usuario_rem, usuario_dest, fecha, contenido, isvisible, isread):
+        self.id_mensaje = id_mensaje
+        self.usuario_rem = usuario_rem
+        self.usuario_dest = usuario_dest
+        self.fecha = fecha
+        self.contenido = contenido
+        self.isvisible = isvisible
+        self.isread = isread
+
+class Chat(db.Model):
+    __tablename__ = "chat"
+
+    mensaje_id = db.Column(db.Integer, db.ForeignKey('mensaje.id_mensaje'), nullable=False)
+    mod_id = db.Column(db.Integer, db.ForeignKey('mod.id_mod'), nullable=False)
+
+    mensaje = db.relationship('Mensaje', backref=db.backref('chat', lazy=True))
+    mod = db.relationship('Mod', backref=db.backref('chat', lazy=True))
+
+    def __init__(self, id_mensaje, id_mod) -> None:
+        self.mensaje_id = id_mensaje
+        self.mod_id = id_mod
+
+class documento(db.Models):
+    usuario_name = db.Column(db.String(255), db.ForeignKey('perfil.usuario'), nullable=False)
+    link = db.Column(db.String(255), nullable=False)#req
+    isaprobado = db.Column(db.Boolean, nullable=False, default=False)
+    tipo = db.Column(db.String(255), nullable=False)#req
+    mod_id = db.Column(db.Integer, db.ForeignKey('mod.id_mod'), nullable=False)
+
+    def __init__(self,usuario_name,link,isaprobado,tipo,mod_id):
+        self.usuario_name = usuario_name
+        self.link = link
+        self.isaprobado = isaprobado
+        self.tipo = tipo
+        self.mod_id = mod_id
